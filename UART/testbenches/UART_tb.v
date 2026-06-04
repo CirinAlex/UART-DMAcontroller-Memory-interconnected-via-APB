0cd1e3f90f030ea27376ca015ccce578bc77995e
CirinAlex <cirinalex17@gmail.com>
2026-06-05 02:34:10 +0530

    F

--- UART/testbenches/UART_tb.v
+++ UART/testbenches/UART_tb.v
@@ -1,4 +1,10 @@
-
+`include ".\modules\clk_divider.v"
+`include ".\modules\UARTtop.v"
+`include ".\modules\uart_timer.v"
+`include ".\modules\RX\RXshift.v"
+`include ".\modules\RX\RXtop.v"
+`include ".\modules\TX\TXshift.v"
+`include ".\modules\TX\TXtop.v"
 
 module uart_tb;
 
@@ -7,7 +13,6 @@ reg[7:0] timerInitVal;
 reg[7:0] TXbuff;
 reg TE;
 wire TI;
-reg TI_in;
 wire TX;
 
 reg pwrite;
@@ -16,13 +21,14 @@ wire[7:0] RXbuff;
 reg RE;
 wire RI;
 reg RI_in;
+reg TI_in;
 reg RX;
 
 
 reg start;
 reg[9:0] RXdata;
 integer i;
-
+integer co = 0;
 
 
 uart dut(.master_clk(master_clk), .timerInitVal(timerInitVal), .TXbuff(TXbuff), .TE(TE), .TI(TI), .TI_in(TI_in), .TX(TX), .pwrite(pwrite), .RXbuff(RXbuff), .RE(RE), .RI(RI), .RI_in(RI_in), .RX(RX));
@@ -31,21 +37,80 @@ uart dut(.master_clk(master_clk), .timerInitVal(timerInitVal), .TXbuff(TXbuff), 
 // drives TX
 always @(start, TI)
 begin
+if(co==0)
+begin
+co = 1;
 	pwrite = 1;
 	TE = 1;
-	#1;
+	#3;
 	TI_in = 1;
-	#1;
-	TI_in = 0;
 	#5;
 	TXbuff = 8'b00001101;
 	pwrite = 0;
-	
-	if(TI==1)
+end
+
+
+if(TI==1 && co==1)
+begin
 		#5;
+		pwrite = 1;
+		#1;
+
 		TXbuff = 8'b01110011;
-		#3;
-		TIin <= 0;
+		TI_in = 0;
+		#2;
+		TI_in = 1;
+		#10;
+		pwrite = 0;
+		co = 2;
+
+end
+if(TI==1 && co==2)
+begin
+		#5;
+		pwrite = 1;
+		#800;
+
+		TXbuff = 8'b01000101;
+		TI_in = 0;
+		#2;
+		TI_in = 1;
+		#200;
+		pwrite = 0;
+		co = 3;
+
+end
+if(TI==1 && co==3)
+begin
+		#5;
+		pwrite = 1;
+		#800;
+
+		TXbuff = 8'b00000011;
+		TI_in = 0;
+		#2;
+		TI_in = 1;
+		#200;
+		pwrite = 0;
+		co = 4;
+
+end
+
+if(TI==1 && co==4)
+begin
+		#5;
+		pwrite = 1;
+		#800;
+
+		TXbuff = 8'b00000000;
+		TI_in = 0;
+		#2;
+		TI_in = 1;
+		#200;
+		pwrite = 0;
+		co = 5;
+
+end
 
 end
 
@@ -70,11 +135,15 @@ begin
 for(integer i=0; i<10; i = i+1)
 begin
 
+if(i!=9)
 	j = 0;
 
 	RX = RXdata[i];
-	for(integer j=0; j<dly; j = j+1)
+	while(j<dly)
+		begin
 		#64;
+		j = j+1;
+		end
 
 end
 
@@ -88,10 +157,15 @@ end
 for(integer i=0; i<10; i = i+1)
 begin
 
+if(i!=9)
 	j = 0;
+
 	RX = RXdata[i];
-	for(integer j=0; j<dly; j = j+1)
+	while(j<dly)
+		begin
 		#64;
+		j = j+1;
+		end
 
 end
 
